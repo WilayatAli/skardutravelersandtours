2eb6b3eb057c2b1a2fe5c539105ed3dc44a0d2f8
WilayatAli <wilayatbalti@gmail.com>
2025-04-13 01:23:02 +0500

    update meta tags and others ui issues

--- assets/js/jquery.counterup.js
+++ assets/js/jquery.counterup.js
@@ -1,25 +1,12 @@
-/*!
-* jquery.counterup.js 1.0
-*
-* Copyright 2013, Benjamin Intal http://gambit.ph @bfintal
-* Released under the GPL v2 License
-*
-* Date: Nov 26, 2013
-*/
 (function( $ ){
   "use strict";
-
   $.fn.counterUp = function( options ) {
-
-    // Defaults
     var settings = $.extend({
         'time': 400,
         'delay': 10
     }, options);
 
     return this.each(function(){
-
-        // Store the object
         var $this = $(this);
         var $settings = settings;
 
@@ -32,32 +19,20 @@
             var isInt = /^[0-9]+$/.test(num);
             var isFloat = /^[0-9]+\.[0-9]+$/.test(num);
             var decimalPlaces = isFloat ? (num.split('.')[1] || []).length : 0;
-
-            // Generate list of incremental numbers to display
             for (var i = divisions; i >= 1; i--) {
-
-                // Preserve as int if input was int
                 var newNum = parseInt(num / divisions * i);
-
-                // Preserve float if input was float
                 if (isFloat) {
                     newNum = parseFloat(num / divisions * i).toFixed(decimalPlaces);
                 }
-
-                // Preserve commas if input had commas
                 if (isComma) {
                     while (/(\d+)(\d{3})/.test(newNum.toString())) {
                         newNum = newNum.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
                     }
                 }
-
                 nums.unshift(newNum);
             }
-
             $this.data('counterup-nums', nums);
             $this.text('0');
-
-            // Updates the number until we're done
             var f = function() {
                 $this.text($this.data('counterup-nums').shift());
                 if ($this.data('counterup-nums').length) {
